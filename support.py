e99375f828bf15dd633a91144afd542f0b42bba8
paris-ci <jovart.arthur@gmail.com>
2016-05-30 22:02:41 +0200

    Beaucoup plus de choix

--- support.py
+++ support.py
@@ -1,41 +1,136 @@
-# -*- coding:Utf-8 -*-
-# !/usr/bin/env python3.5
+# coding: utf8
+#!/usr/bin/env python2.7
 """
 support -- support.py
 MODULE DESC 
 """
 # Constants #
+from __future__ import unicode_literals
 
 __author__ = "Arthur — paris-ci"
 __licence__ = "WTFPL — 2016"
+
 
 import sys
 import locale
 
 
 try:
+    # noinspection PyUnresolvedReferences
     from dialog import Dialog
 except ImportError:
     print("Vous n'avez pas décompréssé l'archive correctement : pythonDialog n'a pas été trouvé :(")
-    sys.exit(1)
+    sys.exit(100)
+
+def propositionsMessages(d, programme):
+    d.msgbox("Bon, passons aux choses serieuses...")
+    if programme == "serveur":
+        if d.yesno("Es-ce que le message parle de permission refusée (permission denied)", yes_label="Oui !", no_label="Non :(") == d.OK:
+            if d.yesno("Super ! Ce message apparait-il quand vous essayez de lancer un programme", yes_label="Oui !", no_label="Non :(")== d.OK:
+                d.msgbox("La solution est toute trouvée : il faut rendre le programme éxécutable avec la commande chmod +x monprogramme, ou le lancer avec l'interpréteur adéquat (exemple, pour un programme python : python monprogramme.py)")
+
+
+
 
 def main():
     locale.setlocale(locale.LC_ALL, '')
-    d = Dialog(dialog="dialog")
+    d = Dialog(dialog="dialog", autowidgetsize=True)
     d.set_background_title("Programme de support express")
     d.msgbox("Bienvenue sur le programme de support technique. Celui-ci vous guidera pour résoudre votre probleme ou la soumettre de maniere correcte !")
-    code, tag = d.menu("Quel est le programme concerné par le probleme ?:",
-                       choices=[("1", "Mon serveur en entier, le soucis est global, ou n'est pas lié à un programme particulier."),
-                                ("2", "Mon site web (apache2, nginx, ioncube...)"),
-                                ("3", "Un serveur de jeu Minecraft"),
-                                ("4", "Un serveur de jeu CS:GO, TF2, ou autre jeu source"),
-                                ("5", "Un VPN OpenVpn"),
-                                ("6", "Le SSHd (connexion au serveur avec PuTTy ou équivalent)"),
-                                ("7", "Une base de donnée (MySql, phpmyadmin...)"),
-                                ("8", "Autre chose, qui n'est pas expliqué ici"),
+    code, programme = d.menu("Quel est le programme concerné par le probleme ?:",
+                       choices=[("serveur", "Mon serveur en entier, le soucis est global, ou n'est pas lié à un programme particulier."),
+                                ("web", "Mon site web (apache2, nginx, ioncube...)"),
+                                ("minecraft", "Un serveur de jeu Minecraft"),
+                                ("source", "Un serveur de jeu CS:GO, TF2, ou autre jeu source"),
+                                ("vpn", "Un VPN OpenVpn"),
+                                ("ssh", "Le SSHd (connexion au serveur avec PuTTy ou équivalent)"),
+                                ("bdd", "Une base de données (MySql, phpmyadmin...)"),
+                                ("autre", "Autre chose, qui n'est pas expliqué ici")
                                 ])
+
     if code == d.OK:
-        pass
+        if d.yesno("Le principal, avez vous lu les logs de l'application ou du service ?", yes_label="Bien sur que oui !", no_label="Euh... On peut le faire maintenent ?") == d.OK:
+            if d.yesno("Et vous n'avez rien trouvé dedans?", yes_label="Sisi, j'ai vu des erreurs", no_label="Vraiment rien trouvé!") == d.OK:
+                if programme == "minecraft":
+                    if d.yesno("Hum... S'agit t'il d'un trés long message avec des at à quasiment chaque ligne ?", yes_label="Oui, c'est ca !", no_label="Non !") == d.OK:
+                        d.msgbox("C'est une stacktrace. Il doit y avoir le nom d'un ou plusieurs plugins dedans, ainsi que leur version. Essaye de les mettre à jour.")
+                        if d.yesno("La mise à jour et le redémarrage ont-ils résolu le probleme ?", yes_label="Oui, merci !", no_label="Non :'(") == d.OK:
+                            d.msgbox("Dans, ce cas, merci à toi ! :)")
+                            sys.exit(0)
+                        else:
+                            propositionsMessages(d, programme)
+                    else:
+                        d.msgbox("Pas grave, nous allons trouver, alors dit moi :")
+
+                code, tag = d.menu("Quel genre de messages ?:",
+                                   choices=[("1", "Des messages d'erreurs"),
+                                            ("2", "Des \"Warnings\""),
+                                            ("3", "Des messsages que j'ai pas trop compris")
+                                            ])
+                tag = int(tag)
+                if code == d.OK:
+                    if tag == 1:
+                        d.msgbox("Super ! La plupart du boulot est fait ! Essayez d'entrer dans google le message d'erreur en retirant la date, l'heure, les chemins de fichiers et tout autre signe distinctif, puis appliquez les solutions proposées")
+                        if d.yesno("Cela à résolu votre probleme ?", yes_label="Oui, merci beaucoup !", no_label="Malheureusement non !") == d.OK:
+                            d.msgbox("Dans, ce cas, merci à toi ! :)")
+                            sys.exit(0)
+                        else:
+                            propositionsMessages(d, programme)
+                    elif tag == 2:
+                        if d.yesno("Le mieux serait d'entrer ces messages sans trop de signes distinctifs dans google et d'essayer les solutions proposées. Est-ce que cela résout le problème ?", yes_label="Oui, merci beaucoup !", no_label="Malheureusement non !") == d.OK:
+                            d.msgbox("Dans, ce cas, merci à toi ! :)")
+                            sys.exit(0)
+                        else:
+                            propositionsMessages(d, programme)
+                    elif tag == 3:
+                        d.msgbox("Essayez d'entrer dans google le message en retirant la date, l'heure, les chemins de fichiers et tout autre signe distinctif, puis appliquez les solutions proposées")
+                        if d.yesno("Cela à résolu votre probleme ?", yes_label="Oui, merci beaucoup !", no_label="Malheureusement non !") == d.OK:
+                            d.msgbox("Dans, ce cas, merci à toi ! :)")
+                            sys.exit(0)
+                        else:
+                            propositionsMessages(d, programme)
+        else:
+            d.msgbox("Pas de soucis, regardons. Tu dois chercher des lignes contenant des erreurs")
+            if d.yesno("Connais-tu le chemin vers ces logs ? Je ne connais que ceux par défault, et si tes serveurs sont installés a d'autres endroits, je ne les trouverai pas.", yes_label="Oui, je connais!", no_label="Malheureusement non !") != d.OK:
+                d.msgbox("Voici les logs, si je les trouves...")
+                if programme == "serveur":
+                    d.textbox("/var/log/syslog")
+                elif programme == "web":
+                    d.textbox("/var/log/apache2/error.log")
+                elif programme == "minecraft":
+                    code, serv = d.inputbox("Quel est le nom du serveur aprés /home/minecraft ? (tapez juste / si il n'y en a pas)", init="/")
+                    d.textbox("/home/minecraft/" + serv + "/logs/latest.log")
+                elif programme == "source":
+                    d.msgbox("Les logs pour les serveurs source sont dans la console seulement")
+                elif programme == "vpn":
+                    d.textbox("/var/log/syslog") # oui, ca log dans syslog openvpn :o
+                elif programme == "sshd":
+                    d.textbox("/var/log/auth.log")
+                    d.textbox("/var/log/messages")
+                elif programme == "bdd":
+                    d.textbox("/var/log/mysql.log")
+                else:
+                    d.msgbox("Le mieux serait de chercher ce chemin sur google...")
+                    code, logs = d.inputbox("Quel est ce chemin ?", init="/var/log/[...]")
+                    if code == d.OK:
+                        d.textbox(logs)
+                    else:
+                        d.msgbox("Au revoir è_é!")
+                        sys.exit(2)
+            else:
+                code, logs = d.inputbox("Quel est ce chemin ?", init="/var/log/[...]")
+                if code == d.OK:
+                    d.textbox(logs)
+                else:
+                    d.msgbox("Au revoir è_é!")
+                    sys.exit(2)
+            d.msgbox("Avec les logs, tu devrais pouvoir résoudre pas mal de problémes, essaye de googler chaque message que tu ne comprends pas... Si ca ne suffit pas, relance le programme")
+            sys.exit(0)
+
+
+
+    else:
+        d.msgbox("Au revoir è_é!")
 
 
 if __name__ == '__main__':
